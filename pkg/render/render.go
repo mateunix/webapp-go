484a1329bfd2351483c73dcc545ea69dcdb19680
mateunix <mateusagre@yandex.com>
2024-11-27 09:03:14 -0300

    adjusting handlers.go

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,8 +6,8 @@ import (
 	"text/template"
 )
 
-// renderTemplate render templates using html/template
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+// RenderTemplate render templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
